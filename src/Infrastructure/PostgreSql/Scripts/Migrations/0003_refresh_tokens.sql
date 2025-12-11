ba984fa846cd143af1052ea8fa07ea0a245c828d
G Allajmi <ghaith.ger@gmail.com>
2025-12-11 16:22:36 +0000

    Refactor tables

--- src/Infrastructure/PostgreSql/Scripts/Migrations/0003_refresh_tokens.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE refresh_tokens (
-    user_id integer primary key REFERENCES users(id) ON DELETE CASCADE,
-    token text NOT NULL,
-    expires_at timestamp with time zone NOT NULL
-);
